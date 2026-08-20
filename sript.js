fcf815fb28158ddff01c119c6837c6b728a01961
BlaZe-I <kifl3994@gmail.com>
2026-08-20 13:01:42 +0200

    changed

--- sript.js
+++ sript.js
@@ -1,9 +1,11 @@
 const picture = [
-    "./asset/img/planet.jpg",
-    "./asset/img/planet.jpg",
-    "./asset/img/planet.jpg",
-    "./asset/img/planet.jpg",
-    "./asset/img/planet.jpg"
+    "./asset/img/amphiebie.png",
+    "./asset/img/fennek.png",
+    "./asset/img/g36.png",
+    "./asset/img/kolone.png",
+    "./asset/img/liebe.jpg",
+    "./asset/img/perfekt.jpg",
+    "./asset/img/soldat.png"
 ]
 
 
@@ -11,7 +13,7 @@ pictureRender()
 
 
 function pictureRender(){
-  document.getElementById("picture").innerHTML = `<img src="${picture[0]}" alt="Bild von Fotogram"></img>`
+  document.getElementById("picture").innerHTML = `<img src="${picture[0]}" alt="Bild von Bundeswehr Ausrüstung und Fahrzeufgen"></img>`
       let bilder = "";
 
     for (let i = 0; i < picture.length; i++) {
